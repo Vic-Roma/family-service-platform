3dd1592d3758ca25d26cba31eb0069202ece72f8
Vic-Roma <vhrm110700@hotmail.com>
2026-06-22 05:30:37 -0600

    Remove Member entity and related API components for cleanup and simplification

--- src/main/java/com/victorhugo/familyservicemanager/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.victorhugo.familyservicemanager.repository;
-
-import com.victorhugo.familyservicemanager.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
